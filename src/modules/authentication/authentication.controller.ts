d74d8ceb9cc9cc3dd0bc4a68f31764d4008620c0
BekmurodFoziloff <bekmurodfoziloff@gmail.com>
2026-01-12 16:57:22 +0500

    feat(auth): implement authentication and authorization

---
+++ src/modules/authentication/authentication.controller.ts
@@ -0,0 +1,62 @@
+import { Body, Req, Controller, HttpCode, Post, Get, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import RegisterDto from './dto/register.dto';
+import { LocalAuthnticationGuard } from './guards/local-authentication.guard';
+import RequestWithUser from './interfaces/request-with-user.interface';
+import JwtAuthenticationGuard from './guards/jwt-authentication.guard';
+import { UsersService } from '../users/users.service';
+import JwtRefreshGuard from './guards/jwt-refresh.guard';
+import MongooseClassSerializerInterceptor from '@common/utils/mongoose-class-serializer.interceptor';
+import { User } from '../users/users.schema';
+
+@Controller('authentication')
+@UseInterceptors(MongooseClassSerializerInterceptor(User))
+export class AuthenticationController {
+  constructor(
+    private readonly authenticationService: AuthenticationService,
+    private readonly usersService: UsersService
+  ) {}
+
+  @Post('register')
+  async register(@Body() registrationData: RegisterDto) {
+    return this.authenticationService.register(registrationData);
+  }
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthnticationGuard)
+  @Post('log-in')
+  async logIn(@Req() request: RequestWithUser) {
+    const { user } = request;
+    const accessTokenCookie = this.authenticationService.getCookieWithJwtAccessToken(user._id);
+    const { cookie: refreshTokenCookie, token: refreshToken } = this.authenticationService.getCookieWithJwtRefreshToken(
+      user._id
+    );
+    await this.usersService.setCurrentRefreshToken(refreshToken, user._id);
+    request.res.setHeader('Set-Cookie', [accessTokenCookie, refreshTokenCookie]);
+    user.password = undefined;
+    user.currentHashedRefreshToken = undefined;
+    return user;
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Post('log-out')
+  @HttpCode(200)
+  async logOut(@Req() request: RequestWithUser) {
+    await this.usersService.removeRefreshToken(request.user._id);
+    request.res.setHeader('Set-Cookie', this.authenticationService.getCookiesForLogOut());
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Get()
+  authenticate(@Req() request: RequestWithUser) {
+    return request.user;
+  }
+
+  @UseGuards(JwtRefreshGuard)
+  @Get('refresh')
+  refresh(@Req() request: RequestWithUser) {
+    const accessTokenCookie = this.authenticationService.getCookieWithJwtAccessToken(request.user._id);
+    request.res.setHeader('Set-Cookie', accessTokenCookie);
+    return request.user;
+  }
+}
---
+++ src/modules/users/users.schema.ts
@@ -0,0 +1,43 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, ObjectId } from 'mongoose';
+import { Exclude, Transform } from 'class-transformer';
+import { WithStringId } from '@common/types/mongo.types';
+
+export type UserDocument = WithStringId<User> & Document;
+
+@Schema({
+  toJSON: {
+    getters: true,
+    virtuals: true
+  },
+  timestamps: true
+})
+export class User {
+  @Transform(({ value }) => value.toString())
+  _id: ObjectId;
+
+  @Prop({ type: String, unique: true, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  firstName: string;
+
+  @Prop({ type: String })
+  lastName: string;
+
+  @Prop({ type: String, required: true })
+  @Exclude()
+  password: string;
+
+  @Prop({ type: String })
+  @Exclude()
+  currentHashedRefreshToken: string;
+}
+
+const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.virtual('fullName').get(function (this: UserDocument) {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+export { UserSchema };
